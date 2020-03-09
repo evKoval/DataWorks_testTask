9e48cb058d53b773f44fdf180d2bb4d48f73339b
Evgeny Koval <evgenekoval@gmail.com>
2020-03-10 02:39:56 +0700

    Updates

---
+++ precache-manifest.61cde3412458477ee9c5bdb8e132b4db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24a82534cf050fa741668355dfe6c943",
+    "url": "/DataWorks_testTask/index.html"
+  },
+  {
+    "revision": "46e63ea06e5c5f41e29f",
+    "url": "/DataWorks_testTask/static/css/main.fea80793.chunk.css"
+  },
+  {
+    "revision": "8d82aa1ac51c8e669f43",
+    "url": "/DataWorks_testTask/static/js/2.3a4afc2c.chunk.js"
+  },
+  {
+    "revision": "e2eae84c7a1d31e04c786e0eb21cdeb1",
+    "url": "/DataWorks_testTask/static/js/2.3a4afc2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46e63ea06e5c5f41e29f",
+    "url": "/DataWorks_testTask/static/js/main.114e1999.chunk.js"
+  },
+  {
+    "revision": "36cebd29e24b43f7558e",
+    "url": "/DataWorks_testTask/static/js/runtime-main.22ae4408.js"
+  }
+]);
